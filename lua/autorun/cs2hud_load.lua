dc8f118b3588714eb935028606065a452242f5fb
Shadow1471 <149724492+Shadow1471@users.noreply.github.com>
2023-11-03 19:37:07 +0100

    Update

--- lua/autorun/cs2hud_load.lua
+++ lua/autorun/cs2hud_load.lua
@@ -1,7 +1,10 @@
 cs2hud = cs2hud or {}
 
-resource.AddFile("resource/fonts/Goodland_SemiBold.ttf")
 print("CS2 HUD - 1.0 Loaded")
+
+-- Font
+resource.AddFile("resource/fonts/Goodland_SemiBold.ttf")
+
 -- Include shared
 include("cs2hud/shared/sh_config.lua")
 
@@ -11,10 +14,10 @@ AddCSLuaFile("cs2hud/shared/sh_config.lua")
 
 -- AddCSLuaFile Client
 AddCSLuaFile("cs2hud/client/vgui/avatar.lua")
-AddCSLuaFile("cs2hud/client/hud.lua")
+AddCSLuaFile("cs2hud/client/cl_cs2hud.lua")
 
 elseif CLIENT then 
 -- include client
 include("cs2hud/client/vgui/avatar.lua")
-include("cs2hud/client/hud.lua")
+include("cs2hud/client/cl_cs2hud.lua")
 end
