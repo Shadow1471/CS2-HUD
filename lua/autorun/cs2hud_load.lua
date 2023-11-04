06012fbda7c07fcfe7d6e4c277bbe1224a23cff8
Shadow1471 <149724492+Shadow1471@users.noreply.github.com>
2023-11-05 00:03:42 +0100

    Add files via upload

--- lua/autorun/cs2hud_load.lua
+++ lua/autorun/cs2hud_load.lua
@@ -1,6 +1,6 @@
 cs2hud = cs2hud or {}
 
-print("CS2 HUD - 1.0 Loaded")
+print("CS2 HUD - 1.1 Loaded")
 
 -- Font
 resource.AddFile("resource/fonts/Goodland_SemiBold.ttf")
@@ -12,12 +12,18 @@ if SERVER then
 -- AddCSLuaFile shared
 AddCSLuaFile("cs2hud/shared/sh_config.lua")
 
+-- Server File
+include("cs2hud/server/sv_cs2hud.lua")
+AddCSLuaFile("cs2hud/server/sv_cs2hud.lua")
+
 -- AddCSLuaFile Client
 AddCSLuaFile("cs2hud/client/vgui/avatar.lua")
 AddCSLuaFile("cs2hud/client/cl_cs2hud.lua")
+AddCSLuaFile("cs2hud/client/cl_cs2hud_panel.lua")
 
 elseif CLIENT then 
 -- include client
 include("cs2hud/client/vgui/avatar.lua")
 include("cs2hud/client/cl_cs2hud.lua")
+include("cs2hud/client/cl_cs2hud_panel.lua")
 end
