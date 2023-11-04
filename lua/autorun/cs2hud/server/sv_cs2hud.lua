2761bef3a7a8904eae3ca0981a0911219011b309
Shadow1471 <149724492+Shadow1471@users.noreply.github.com>
2023-11-05 00:03:26 +0100

    Add files via upload

---
+++ lua/autorun/cs2hud/server/sv_cs2hud.lua
@@ -0,0 +1,4 @@
+
+CreateClientConVar( "color_of_the_cs2_hud_r", "255", true, false, "Set Red Color of the HUD" )
+CreateClientConVar( "color_of_the_cs2_hud_g", "255", true, false, "Set Green Color of the HUD" )
+CreateClientConVar( "color_of_the_cs2_hud_b", "255", true, false, "Set Blue Color of the HUD" )
